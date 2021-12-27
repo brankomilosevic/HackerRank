d2a7057136d04926bc46ef5b9b88363a111b923e
Branko Milosevic <63554382+brankomilosevic@users.noreply.github.com>
2021-12-27 14:25:20 +0100

    Add files via upload

---
+++ cats_and_mouse.cpp
@@ -0,0 +1,84 @@
+/*
+HACKERRANK: Cats and Mouse
+https://www.hackerrank.com/challenges/cats-and-a-mouse
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the catAndMouse function below.
+string catAndMouse(int x, int y, int z) {
+    int dist_a = x - z; 
+        if (dist_a < 0) dist_a = -dist_a;
+    int dist_b = y - z;
+        if (dist_b < 0) dist_b = -dist_b;
+    
+    if (dist_a < dist_b)
+        return string("Cat A");
+    if (dist_b < dist_a)
+        return string("Cat B");
+    return string("Mouse C");
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string xyz_temp;
+        getline(cin, xyz_temp);
+
+        vector<string> xyz = split_string(xyz_temp);
+
+        int x = stoi(xyz[0]);
+
+        int y = stoi(xyz[1]);
+
+        int z = stoi(xyz[2]);
+
+        string result = catAndMouse(x, y, z);
+
+        fout << result << "\n";
+        cout << "Solution: " << result;
+    }
+
+    fout.close();
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
---
+++ magic_square.cpp
@@ -0,0 +1,190 @@
+/*
+HACKERRANK: MagicSquare
+https://www.hackerrank.com/challenges/magic-square-forming
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'formingMagicSquare' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY s as parameter.
+ */
+
+int sum_3(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+int my_abs(int a)
+{
+    if (a < 0) 
+        return -a;
+    return a;
+}
+
+int formingMagicSquare_old(vector<vector<int>> s) {
+    int TOTAL = 15;
+    int cost = 0;
+
+    int hor = 0;
+    int ver = 0;
+    int diag = 0;
+
+    for (int i=0; i<3; ++i)
+    {
+        int sum = 0;
+        for (int j=0; j<3; ++j)
+            sum += s[i][j];
+        hor += abs(TOTAL - sum);
+    }
+
+    for (int i=0; i<3; ++i)
+    {
+        int sum = 0;
+        for (int j=0; j<3; ++j)
+            sum += s[j][i];
+        ver += abs(TOTAL - sum);
+    }
+
+    int sum = 0;
+    for (int i=0; i<3; ++i)
+        sum += s[i][i];
+    diag += abs(TOTAL - sum);
+
+    if (hor >= ver && hor >= diag)
+        return hor;
+
+    if (ver >= hor && ver >= diag)
+        return ver;
+
+    if (diag >= ver && diag >= hor)
+        return diag;
+    
+    return -1;
+}
+
+void print_array(int a[])
+{
+    cout << "\n";
+    for (int i=0; i<9; ++i)
+        cout << a[i] << " ";
+    
+}
+int formingMagicSquare(vector<vector<int>> s) {
+    int cost = 1000;
+    int min_cost = 1000;
+
+    int arr[9];
+    int perm[9];
+
+    for (int i=0; i<3; ++i)
+        for (int j=0; j<3; ++j)
+            arr[i*3+j] = s[i][j];
+
+    for (int i=0; i<9; ++i)
+        perm[i] = i+1;
+
+    do 
+    {
+        if ( perm[0] + perm[1] + perm[2] == 15 &&
+             perm[3] + perm[4] + perm[5] == 15 &&
+             perm[6] + perm[7] + perm[8] == 15 &&
+             perm[0] + perm[3] + perm[6] == 15 &&
+             perm[1] + perm[4] + perm[7] == 15 &&
+             perm[2] + perm[5] + perm[8] == 15 &&
+             perm[0] + perm[4] + perm[8] == 15 &&
+             perm[2] + perm[4] + perm[6] == 15 
+           )
+        {
+            cost = 0;
+            for (int i=0; i<9; ++i)
+                cost += my_abs(arr[i] - perm[i]);
+            if (min_cost > cost)
+            {
+                min_cost = cost;
+                print_array(arr);
+                print_array(perm);
+                cout << "\n";
+            }
+        }
+    } 
+    while (next_permutation(perm, perm + 9));
+    return min_cost;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> s(3);
+
+    for (int i = 0; i < 3; i++) {
+        s[i].resize(3);
+
+        string s_row_temp_temp;
+        getline(cin, s_row_temp_temp);
+
+        vector<string> s_row_temp = split(rtrim(s_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int s_row_item = stoi(s_row_temp[j]);
+
+            s[i][j] = s_row_item;
+        }
+    }
+
+    int result = formingMagicSquare(s);
+
+    fout << result << "\n";
+
+    fout.close();
+    cout << "Solution: " << result;
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
