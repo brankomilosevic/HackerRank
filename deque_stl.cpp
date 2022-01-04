eb72d6cf8060d79b6739a60ff59d80e79358f3ca
Branko Milosevic <63554382+brankomilosevic@users.noreply.github.com>
2022-01-04 14:50:46 +0100

    Add files via upload

--- deque_stl.cpp
+++ deque_stl.cpp
@@ -13,38 +13,38 @@ using namespace std;
 
 void printKMax(int arr[], int n, int k)
 {
-	//Write your code here.
-   deque<int> dq;
-   vector<int> res;
+    //Write your code here.
+    deque<int> dq;
+    vector<int> res;
 
-   // for First K elements
-   for( int i = 0; i < k; ++i)
-   {
-       while( !dq.empty() && arr[i] >= arr[dq.back()])
-           // Remove the indices of elements that are smaller than the current elements
-           dq.pop_back();
-       // add as a maximum    
-       dq.push_back(i);
-   }
-   // the element at the front has index of the highest element in the window
-   res.push_back(arr[dq.front()]);
-   // for the rest of the array
-   for ( int i = k; i < n; ++i )
-   {
+    // for First K elements
+    for( int i = 0; i < k; ++i)
+    {
+        while( !dq.empty() && arr[i] >= arr[dq.back()])
+        // Remove the indices of elements that are smaller than the current elements
+            dq.pop_back();
+        // add as a maximum    
+        dq.push_back(i);
+    }
+    // the element at the front has index of the highest element in the window
+    res.push_back(arr[dq.front()]);
+    // for the rest of the array
+    for ( int i = k; i < n; ++i )
+    {
         // drop the elements that are out of window
         while(!dq.empty() && dq.front() <= i-k)
             dq.pop_front();
-        
+
         // repeat what we have done for first k elements
         while(!dq.empty() && arr[i] >= arr[dq.back()])
             dq.pop_back();  
         dq.push_back(i);
         res.push_back(arr[dq.front()]);
-   }   
-   
-   for (int i : res)
-    cout << i << " ";
-    
+    }   
+
+    for (int i : res)
+        cout << i << " ";
+    cout << endl;
 }
 
 int main(){
